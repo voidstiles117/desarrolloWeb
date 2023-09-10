b1aa3f1720b1b0bbda744fd47f01da8ad97a40ef
Void Stiles <danielgona101@gmail.com>
2023-09-10 16:40:57 -0500

    Agregamos validacionnes mediante ts

--- login/login.js
+++ login/login.js
@@ -1,32 +1,29 @@
-// capturamos los datos 
-const parrafo = document.getElementById("warning")
-
-form.addEventListener("submit", e => {
-  e.preventDefault()
-
-  const usuario = document.getElementById("input-usuario").value
-  const contrasena = document.getElementById("input-contrasena").value
-
-  let errores = ""
-
-  errores += validaUsuario(usuario);
-  errores += validaContrasena(contrasena);
-
-  if (errores) {
-    parrafo.innerHTML = errores;
-  }
-})
-
-const validaUsuario = (usuarioElemento) => {
-  if (usuarioElemento === "") {
-    return `<p class="has-text-centered has-text-danger" id="warning">Nombre esta vacio</p>`;
-  }
-  return ""
+var parrafo = document.getElementById("warning");
+var form = document.querySelector("form");
+if (form) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        var usuarioElement = document.getElementById("input-usuario");
+        var contrasenaElement = document.getElementById("input-contrasena");
+        var errores = "";
+        if (usuarioElement && contrasenaElement) {
+            errores += validaUsuario(usuarioElement.value);
+            errores += validaContrasena(contrasenaElement.value);
+        }
+        if (errores && parrafo) {
+            parrafo.innerHTML = errores;
+        }
+    });
 }
-
-const validaContrasena = (contrasenaElemento) => {
-  if (contrasenaElemento === "") {
-    return `<p class="has-text-centered has-text-danger" id="warning">Esta vacio</p>`;
-  }
-  return ""
-}
+var validaUsuario = function (usuarioElemento) {
+    if (usuarioElemento === "") {
+        return "<p class=\"has-text-centered has-text-danger\" id=\"warning\">Nombre esta vacio</p>";
+    }
+    return "";
+};
+var validaContrasena = function (contrasenaElemento) {
+    if (contrasenaElemento === "") {
+        return "<p class=\"has-text-centered has-text-danger\" id=\"warning\">Esta vacio</p>";
+    }
+    return "";
+};
